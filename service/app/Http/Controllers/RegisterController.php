bfda1efbdc1b5143691b624075da50b209b9880f
KouheiMiki <kouhei.miki0926@gmail.com>
2023-12-10 15:35:05 +0900

    add mail

--- service/app/Http/Controllers/RegisterController.php
+++ service/app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -26,6 +27,8 @@ class RegisterController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        event(new Registered($user));
 
         return view('regist.complete', compact('user'));
     }
